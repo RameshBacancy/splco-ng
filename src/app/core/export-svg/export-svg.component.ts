553199fc20eaaf2f6e64929899f35a67dc2cb634
RameshBacancy <ramesh.prajapati@bacancytechnology.com>
2020-05-01 19:03:26 +0530

    download svg to png file, added white background and black edges after downloaded png file

--- src/app/core/export-svg/export-svg.component.ts
+++ src/app/core/export-svg/export-svg.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ExportSvgToPngService } from 'src/app/service/export-svg-to-png.service';
+import html2canvas from 'html2canvas';
+import { SpinnerService } from 'src/app/service/spinner.service';
 
 @Component({
   selector: 'app-export-svg',
@@ -8,21 +10,32 @@ import { ExportSvgToPngService } from 'src/app/service/export-svg-to-png.service
 })
 export class ExportSvgComponent implements OnInit {
   @Input() dataId;
-  constructor(public exportSvgToPngService: ExportSvgToPngService) { }
+  imgSrc = '';
+
+  constructor(
+    public exportSvgToPngService: ExportSvgToPngService,
+    public spinnerService: SpinnerService,
+  ) { }
 
   ngOnInit(): void { }
 
-
   export(id) {
-    var svgString = document.getElementById(id).innerHTML;
+    var s = document.getElementById(id).getElementsByTagName("svg");
+    s[0].style.fill = "transparent";
+    s[0].style.stroke = "black";
 
-    const data = svgString.match(/<svg.*>(.|\n)*?<\/svg>/g) || [];
-    const svgNewString = data[0];
+   
+    this.spinnerService.openSpinner();
+    this.imgSrc = '';
 
-    document.getElementById('latest-svg').innerHTML = svgNewString;
-    var svgLatestString = document.getElementById('latest-svg').innerHTML;
-    // console.log('svgLatestString', svgLatestString)
-
-    this.exportSvgToPngService.export(svgLatestString);
+    var svgString = document.getElementById(id);
+    html2canvas(svgString).then(canvas => {
+      this.imgSrc = canvas.toDataURL();
+      const anchor = document.createElement("a");
+      anchor.download = 'Ngx-Graph.png';
+      anchor.href = this.imgSrc;
+      anchor.click();
+      this.spinnerService.closeSpinner();
+    });
   }
 }
