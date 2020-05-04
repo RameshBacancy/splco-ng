bdc01ae99926d8013bb69f0694db72c36d2bdc05
RameshBacancy <ramesh.prajapati@bacancytechnology.com>
2020-05-04 20:57:54 +0530

    on click of export button,svg will be download in  svg and  png format.

--- src/app/core/export-svg/export-svg.component.ts
+++ src/app/core/export-svg/export-svg.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, NgZone } from '@angular/core';
 import html2canvas from 'html2canvas';
 import { SpinnerService } from 'src/app/service/spinner.service';
 
@@ -10,26 +10,29 @@ import { SpinnerService } from 'src/app/service/spinner.service';
 export class ExportSvgComponent implements OnInit {
   @Input() dataId;
   imgSrc = '';
+  newSVGString;
 
-  constructor(public spinnerService: SpinnerService) { }
+  constructor(public spinnerService: SpinnerService, public ngZone: NgZone) { }
 
   ngOnInit(): void { }
 
   export(id) {
     this.spinnerService.openSpinner();
     this.imgSrc = '';
+
     let svg = document.querySelector('svg');
     let g = document.querySelector('g');
-    svg.style.fill = "none";
+    svg.style.fill = "white";
     svg.style.stroke = "black";
     g.style.transform = "matrix(1,0,0,1,0,0)";
     setTimeout(() => {
       svg.style.display = "none";
     }, 0);
     this.imgSrc = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(this.imgSrc)));
-    let svgString = document.getElementById(id);
+    let pngString = document.getElementById(id);
+    this.newSVGString = document.getElementById(id).innerHTML;
 
-    html2canvas(svgString).then(canvas => {
+    html2canvas(pngString).then(canvas => {
       this.imgSrc = canvas.toDataURL();
       const anchor = document.createElement("a");
       anchor.download = 'Ngx-Graph.png';
@@ -41,5 +44,42 @@ export class ExportSvgComponent implements OnInit {
       g.style.removeProperty("transform");
       svg.style.display = "block";
     });
+
+    var width = 500, height = 500;
+    let svgTag = (this.newSVGString.match(/<svg .*?>/g) || []);
+    if (svgTag) {
+      width = (svgTag[0].match(/width=".*?"/g) || []);
+      if (width && width[0]) {
+        width = Number(width[0].substring(7, 10));
+      } else {
+        width = 300;
+      }
+      height = (svgTag[0].match(/height=".*?"/g) || []);
+      if (height && height[0]) {
+        height = Number(height[0].substring(8, 11));
+      } else {
+        height = 300;
+      }
+    }
+    this.svgString2Image(this.newSVGString, 2 * width, 2 * height, 'png', (dataBlob, filesize) => this.ngZone.run(() => {
+    }));
+  }
+
+  svgString2Image(pngData, width, height, format, callback) {
+
+    var format = format ? format : 'svg';
+    var imgsrc = 'data:image/svg+xml;base64,' + btoa(unescape(encodeURIComponent(pngData)));
+    var canvas = document.createElement("canvas");
+
+    canvas.width = width;
+    canvas.height = height;
+
+    var dl = document.createElement("a");
+    document.body.appendChild(dl);
+    dl.setAttribute("href", imgsrc);
+    dl.setAttribute("download", "Ngx-Graph.svg");
+    dl.click();
+
+    this.spinnerService.closeSpinner();
   }
 }
